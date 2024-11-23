d357b27a0e2fe56c92af9590e8b2ee2b54513bf6
pasca-l <shion.yamadate@keio.jp>
2024-11-24 00:00:11 +0900

    updated main to access a webpage

--- browser/src/main.rs
+++ browser/src/main.rs
@@ -1,12 +1,16 @@
-#![no_std]
-#![cfg_attr(not(target_os = "linux"), no_main)]
+extern crate alloc;
 
-use noli::prelude::*;
+use crate::alloc::string::ToString;
+use net_std::http::HttpClient;
 
 fn main() {
-    Api::write_string("Hello World\n");
-    println!("Hello from println!");
-    Api::exit(42);
+    let client = HttpClient::new();
+    match client.get("example.com".to_string(), 80, "/".to_string()) {
+        Ok(res) => {
+            println!("response:\n{:#?}", res);
+        }
+        Err(e) => {
+            println!("error:\n{:#?}", e);
+        }
+    }
 }
-
-entry_point!(main);
